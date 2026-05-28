2d390b3d3c4bafd39d136ea315e9a2539e8c001b
Lamia-Ahmed-Khan <105999789@student.swin.edu.au>
2026-05-29 09:35:44 +1000

    Trying to solve merge conflict

--- manage.php
+++ manage.php
@@ -20,7 +20,7 @@ if (!$conn) {
     </style>
 </head>
 <body>
-<?php include 'include/header_management.inc'; ?> 
+<?php include 'include/header_main.inc'; ?> 
 
 <main>
     <!-- Zarin can add logout button here -->
