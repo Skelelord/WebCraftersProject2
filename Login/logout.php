a949247ad259b99eec502a47aa6f34edb3a6098b
ZarinTasnim27 <105981579@student.swin.edu.au>
2026-05-29 16:36:15 +1000

    added comments in logout.php

--- Login/logout.php
+++ Login/logout.php
@@ -1,8 +1,8 @@
 <?php
-// session_start() at top of every page that uses sessions - from lab
+// session_start() at top of every page that uses sessions 
 session_start();
 
-// session_destroy() ends the session during logout - from lab
+// session_destroy() ends the session during logout 
 session_unset();
 session_destroy();
 
